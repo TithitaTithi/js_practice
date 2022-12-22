b2369100ef0cc46f591394ec2bee5e81d5c6c68f
TithitaTithi <tithitatithi84@gmail.com>
2022-12-22 10:22:04 +0600

    added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var num1=(Number(prompt("enter a num")));
+var num2=(Number(prompt("enter a num")));
+var num3=(Number(prompt("enter a num")));
+
+function result(){
+document.write("<br/> sq is " + (num1*num1));
+document.write("<br/> add is " + (num1+num2+num3));
+document.write("<br/> sub is " + (num1-num2 -num3));
+
+}
+result();
